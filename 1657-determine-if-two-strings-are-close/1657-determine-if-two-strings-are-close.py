fc3ccd0432439e9fd6d9f4b44d557a9514b525b7
Keven Beasley <76821317+thebeasknees@users.noreply.github.com>
2022-06-21 18:06:29 -0500

    Time: 273 ms (42.86%), Space: 15.5 MB (20.71%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.py
@@ -2,8 +2,6 @@ class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         if len(word1) != len(word2):
             return False
-      #  a = Counter(word1)
-      #  b = Counter(word2)
         if Counter(word1).keys() != Counter(word2).keys():
             return False
         return sorted(Counter(word1).values()) == sorted(Counter(word2).values())
